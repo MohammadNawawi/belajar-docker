8e8e0cb46e482a5f61270c65f9468b5df9c27dd1
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-12-01 22:22:06 +0700

    first commit

---
+++ docker-container.sh
@@ -0,0 +1,8 @@
+# TODO : Melihat semua docker container di server
+docker container ls -a
+
+# TODO : Melihat semua docker container yang aktif
+docker container ls
+
+# TODO : Membuat Docker Container
+docker container create --name contohredis redis:latest
