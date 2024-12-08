957f5fdfde80898f7bd23626f2e57200c83740e6
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-12-08 17:24:51 +0700

    feat(commad) : add docker container commad

--- docker-container.sh
+++ docker-container.sh
@@ -1,8 +1,17 @@
-# TODO : Melihat semua docker container di server
+# TODO : Look all docker container in server
 docker container ls -a
 
-# TODO : Melihat semua docker container yang aktif
+# TODO : Look all active docker container in server
 docker container ls
 
-# TODO : Membuat Docker Container
+# TODO : Create Docker Container
 docker container create --name contohredis redis:latest
+
+# TODO : Run Docker Container
+docker container start contohredis
+
+# TODO : Stop Docker Container
+docker container stop contohredis
+
+# TODO : Delete Docker Container
+docker container rm contohredis
