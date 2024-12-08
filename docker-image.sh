02750be46f6a23a57e3334148afe2cf74451626a
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-12-08 17:13:07 +0700

    feat(commad) : cocker image

---
+++ docker-image.sh
@@ -0,0 +1,9 @@
+
+# TODO : Look docker image downloaded
+docker image ls
+
+# TODO : Download docker image
+docker image pull redis:latest
+
+# TODO : Delete docker image
+docker image rm redis:latest
